5effb1b378e1f22c7de1049409c8f414488e4665
ykim306 <ykim306@gmail.com>
2019-05-28 14:41:47 -0400

    Added Nightmode swap menu

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
         mScoreText1 = findViewById(R.id.team1scoreLabel);
         mScoreText2 = findViewById(R.id.team2scoreLabel);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.night_mode) {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+
+            recreate();
+        }
+
+        return true;
     }
 
     public void decreaseScore(View view) {
